506e450756f2e4ebf474a6ba3a7bad3aa3876dcf
uidude <22799484+uidude@users.noreply.github.com>
2023-03-18 10:02:39 -0700

    Placeholder TOS

--- minders/client/app/screens/LoginScreen.tsx
+++ minders/client/app/screens/LoginScreen.tsx
@@ -13,12 +13,17 @@ import {
 } from '@toolkit/screens/login/LoginScreen';
 import {Screen} from '@toolkit/ui/screen/Screen';
 
+const TOS =
+  "By continuing, you'll need to accept our " +
+  '[Terms of Service](https://media.istockphoto.com/id/482103289/photo/clown-laywer.jpg?s=612x612&w=0&k=20&c=aHoyN4YAeyzTd5yLyt0WBQskRce-G9rkepA_TX3_RHs=), ' +
+  "although they haven't quite been defined quite yet.";
+
 const LOGIN_SCREEN_CONFIG: SimpleLoginScreenConfig = {
-  title: 'Welcome to minders',
-  subtitle:
-    'This is a Simple Login Screen. \nEdit app/screens/LoginScreen.tsx to edit this!',
+  title: 'Welcome to Minders',
+  subtitle: 'Keep focused!',
   authTypes: ['google', 'phone'],
   home: 'outline',
+  tos: TOS,
 };
 
 const LoginScreen: Screen<{}> = simpleLoginScreen(LOGIN_SCREEN_CONFIG);
