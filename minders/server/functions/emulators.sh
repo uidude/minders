5c6675812ec86e15650d9b6fb8c3f1c6d4dfe3cb
uidude <22799484+uidude@users.noreply.github.com>
2023-05-25 12:43:42 -0700

    Remove checksum logic for now

--- minders/server/functions/emulators.sh
+++ minders/server/functions/emulators.sh
@@ -3,6 +3,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+: '
 OLD_CHECKSUM=$(cat .runtimeconfig_checksum)
 CURRENT_CHECKSUM=$(cksum .runtimeconfig.json)
 if [ "$OLD_CHECKSUM" != "$CURRENT_CHECKSUM" ] || [ ! -f ".runtimeconfig.json" ]
@@ -11,6 +12,7 @@ then
   firebase functions:config:get > .runtimeconfig.json
   cksum functions/.runtimeconfig.json > .runtimeconfig_checksum
 fi
+'
 
 # Download this from Firebase Console, `Settings icon > Service Accounts tab`
 export GOOGLE_APPLICATION_CREDENTIALS=$HOME/.secrets/minders.json
