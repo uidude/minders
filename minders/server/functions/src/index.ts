3d89c39e3fabf9f845f2f0c1dc940b1e5b29d04a
uidude <22799484+uidude@users.noreply.github.com>
2023-06-13 17:56:17 -0700

    Remoe old comment

--- minders/server/functions/src/index.ts
+++ minders/server/functions/src/index.ts
@@ -8,8 +8,6 @@ import {
   providersMiddleware,
 } from '@toolkit/providers/firebase/server/Handler';
 
-// Follow the wiki below to enable Firestore security rule enforcement in Functions:
-// https://www.internalfb.com/intern/wiki/NPE/Central_Engineering/NPE_Kit/Guides/Enforcing_Security_Rules_in_Firebase_Functions_or_Server_Code/
 initFirebaseServer(FIREBASE_CONFIG, APP_CONFIG);
 
 const providers = [FirestoreDatastore, APP_CONFIG];
