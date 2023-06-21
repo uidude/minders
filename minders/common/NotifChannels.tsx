bfed9af93a04faed53112c5c01ecf67b05026d42
uidude <22799484+uidude@users.noreply.github.com>
2023-06-21 16:39:32 -0700

    Update notification channels to Minder-relevant ones

--- minders/common/NotifChannels.tsx
+++ minders/common/NotifChannels.tsx
@@ -2,29 +2,18 @@ import NotificationChannel from '@toolkit/services/notifications/NotificationCha
 
 export const NOTIF_CHANNELS: Record<string, NotificationChannel> = {
   thingFaved: new NotificationChannel({
-    id: 'HAX_APP:THING_FAVED',
-    name: 'Faves',
-    description:
-      "These are notifications for when someone Fave's a Thing you added",
-    titleFormat: 'New Fave',
-    bodyFormat: '${likerName} faved your thing: "${thingName}"',
-    defaultDeliveryMethod: 'PUSH',
-  }),
-
-  thingDeleted: new NotificationChannel({
-    id: 'HAX_APP:THING_DELETED',
-    name: 'Thing Deleted',
-    description:
-      'These are notifications for when someone deletes a Thing you added',
-    titleFormat: 'Thing Deleted',
-    bodyFormat: '"${thingName}" has been deleted',
+    id: 'minders:reminder',
+    name: 'Minder reminders',
+    description: 'Reminders for upcoming Minders',
+    titleFormat: '${title}',
+    bodyFormat: '${body}',
     defaultDeliveryMethod: 'PUSH',
   }),
 
   admin: new NotificationChannel({
-    id: 'HAX_APP:ADMIN',
-    name: 'ADMIN',
-    description: 'These are one-off notifications from admins',
+    id: 'minders:admin',
+    name: 'System notifications',
+    description: 'Notifications from the app',
     titleFormat: '${title}',
     bodyFormat: '${body}',
     defaultDeliveryMethod: 'PUSH',
